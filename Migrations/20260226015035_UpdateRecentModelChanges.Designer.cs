65a010344047d066cffc49347afd52bb76081944
MarcoBailer <mota23@discente.ufg.br>
2026-02-25 22:53:14 -0300

    Adição de migrations ✅

--- Migrations/20260226015035_UpdateRecentModelChanges.Designer.cs
+++ Migrations/20260226015035_UpdateRecentModelChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Nutra.Data;
@@ -11,9 +12,11 @@ using Nutra.Data;
 namespace Nutra.Migrations
 {
     [DbContext(typeof(AlimentosContext))]
-    partial class AlimentosContextModelSnapshot : ModelSnapshot
+    [Migration("20260226015035_UpdateRecentModelChanges")]
+    partial class UpdateRecentModelChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
